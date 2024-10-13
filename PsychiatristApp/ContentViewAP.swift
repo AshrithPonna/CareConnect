ada735b319fc4246f263b4600e518aa5b2be839b
Ashrith Ponna <156397382+AshrithPonna@users.noreply.github.com>
2024-10-13 09:49:41 -0500

    Create ContentViewAP.swift
    Added a segment of code that corroborates user interface with quiz interface properties.

---
+++ PsychiatristApp/ContentViewAP.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// PsychProj
+//
+// Created by Ashrith Ponna on 9/20/24
+//
+
+//Importing libraries to initialize Firebase, and to initialize the basic structure of the app.
+
+//MERGE WITH QVIEW?
+
+import SwiftUI
+import Firebase
+
+// Setting up the quiz infrastructural view for the patient.
+
+struct ContentView: View {
+    @ObservedObject var quizManager = QuizManager()
+    @State private var currentUserID: String = Auth.auth().currentUser?.uid ?? ""
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(quizManager.questions) { question
+
